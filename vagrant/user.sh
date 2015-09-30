ffc4ff42654df6b56f0b4c5ddd37915809d79b50
Sergiy Misyura <sergey@cando.com>
2015-09-30 16:54:57 -0400

    Installing codeception

--- vagrant/user.sh
+++ vagrant/user.sh
@@ -1,4 +1,8 @@
 composer global require "fxp/composer-asset-plugin:~1.0.3"
+composer global require "codeception/codeception=2.0.*"
+composer global require "codeception/specify=*"
+composer global require "codeception/verify=*"
+
 pushd /vagrant
 composer install
 popd
